e3804b0e9a0ac77bc62afe3f33bafd007f8cbb65
NedaKovacevic <neda.n.kovacevic@gmail.com>
2022-03-27 18:11:44 +0200

    menu

---
+++ ednevnik/Glavna.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ednevnik
+{
+    partial class Glavna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jedanBezToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jedanSaFKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelaBezToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izvestajiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.osobeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jedanBezToolStripMenuItem,
+            this.jedanSaFKToolStripMenuItem,
+            this.tabelaBezToolStripMenuItem,
+            this.tabelaToolStripMenuItem,
+            this.izvestajiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1613, 52);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jedanBezToolStripMenuItem
+            // 
+            this.jedanBezToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.osobeToolStripMenuItem});
+            this.jedanBezToolStripMenuItem.Name = "jedanBezToolStripMenuItem";
+            this.jedanBezToolStripMenuItem.Size = new System.Drawing.Size(174, 48);
+            this.jedanBezToolStripMenuItem.Text = "Jedan Bez";
+            this.jedanBezToolStripMenuItem.Click += new System.EventHandler(this.jedanBezToolStripMenuItem_Click);
+            // 
+            // jedanSaFKToolStripMenuItem
+            // 
+            this.jedanSaFKToolStripMenuItem.Name = "jedanSaFKToolStripMenuItem";
+            this.jedanSaFKToolStripMenuItem.Size = new System.Drawing.Size(190, 48);
+            this.jedanSaFKToolStripMenuItem.Text = "Jedan SaFK";
+            // 
+            // tabelaBezToolStripMenuItem
+            // 
+            this.tabelaBezToolStripMenuItem.Name = "tabelaBezToolStripMenuItem";
+            this.tabelaBezToolStripMenuItem.Size = new System.Drawing.Size(181, 48);
+            this.tabelaBezToolStripMenuItem.Text = "Tabela Bez";
+            // 
+            // tabelaToolStripMenuItem
+            // 
+            this.tabelaToolStripMenuItem.Name = "tabelaToolStripMenuItem";
+            this.tabelaToolStripMenuItem.Size = new System.Drawing.Size(197, 48);
+            this.tabelaToolStripMenuItem.Text = "Tabela SaFK";
+            this.tabelaToolStripMenuItem.Click += new System.EventHandler(this.tabelaToolStripMenuItem_Click);
+            // 
+            // izvestajiToolStripMenuItem
+            // 
+            this.izvestajiToolStripMenuItem.Name = "izvestajiToolStripMenuItem";
+            this.izvestajiToolStripMenuItem.Size = new System.Drawing.Size(146, 48);
+            this.izvestajiToolStripMenuItem.Text = "Izvestaji";
+            // 
+            // osobeToolStripMenuItem
+            // 
+            this.osobeToolStripMenuItem.Name = "osobeToolStripMenuItem";
+            this.osobeToolStripMenuItem.Size = new System.Drawing.Size(448, 54);
+            this.osobeToolStripMenuItem.Text = "Osobe";
+            this.osobeToolStripMenuItem.Click += new System.EventHandler(this.osobeToolStripMenuItem_Click);
+            // 
+            // Glavna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1613, 963);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Glavna";
+            this.Text = "Glavna";
+            this.Load += new System.EventHandler(this.Glavna_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jedanBezToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jedanSaFKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabelaBezToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabelaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izvestajiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem osobeToolStripMenuItem;
+    }
+}
---
+++ ednevnik/Glavna.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ednevnik
+{
+    public partial class Glavna : Form
+    {
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void tabelaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void jedanBezToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Glavna_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Osoba frmOsoba = new Osoba();
+            frmOsoba.Show();
+        }
+    }
+}
