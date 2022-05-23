262a24e37e730b83fc1fc70ab6381d493e758f1b
NedaKovacevic <neda.n.kovacevic@gmail.com>
2022-05-23 21:23:19 +0200

    upisnica

---
+++ ednevnik/Upisnica.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ednevnik
+{
+    partial class Upisnica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxGodina = new System.Windows.Forms.ComboBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.comboBoxOdeljenje = new System.Windows.Forms.ComboBox();
+            this.comboBoxUcenik = new System.Windows.Forms.ComboBox();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonIzmeni = new System.Windows.Forms.Button();
+            this.buttonBrisi = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxGodina
+            // 
+            this.comboBoxGodina.FormattingEnabled = true;
+            this.comboBoxGodina.Location = new System.Drawing.Point(434, 189);
+            this.comboBoxGodina.Name = "comboBoxGodina";
+            this.comboBoxGodina.Size = new System.Drawing.Size(283, 39);
+            this.comboBoxGodina.TabIndex = 0;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(145, 189);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(231, 38);
+            this.textBoxId.TabIndex = 1;
+            // 
+            // comboBoxOdeljenje
+            // 
+            this.comboBoxOdeljenje.FormattingEnabled = true;
+            this.comboBoxOdeljenje.Location = new System.Drawing.Point(757, 188);
+            this.comboBoxOdeljenje.Name = "comboBoxOdeljenje";
+            this.comboBoxOdeljenje.Size = new System.Drawing.Size(283, 39);
+            this.comboBoxOdeljenje.TabIndex = 2;
+            // 
+            // comboBoxUcenik
+            // 
+            this.comboBoxUcenik.FormattingEnabled = true;
+            this.comboBoxUcenik.Location = new System.Drawing.Point(1066, 189);
+            this.comboBoxUcenik.Name = "comboBoxUcenik";
+            this.comboBoxUcenik.Size = new System.Drawing.Size(283, 39);
+            this.comboBoxUcenik.TabIndex = 3;
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(332, 300);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(167, 61);
+            this.buttonDodaj.TabIndex = 4;
+            this.buttonDodaj.Text = "Dodaj";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // buttonIzmeni
+            // 
+            this.buttonIzmeni.Location = new System.Drawing.Point(647, 300);
+            this.buttonIzmeni.Name = "buttonIzmeni";
+            this.buttonIzmeni.Size = new System.Drawing.Size(167, 61);
+            this.buttonIzmeni.TabIndex = 5;
+            this.buttonIzmeni.Text = "Izmeni";
+            this.buttonIzmeni.UseVisualStyleBackColor = true;
+            this.buttonIzmeni.Click += new System.EventHandler(this.buttonIzmeni_Click);
+            // 
+            // buttonBrisi
+            // 
+            this.buttonBrisi.Location = new System.Drawing.Point(920, 300);
+            this.buttonBrisi.Name = "buttonBrisi";
+            this.buttonBrisi.Size = new System.Drawing.Size(167, 61);
+            this.buttonBrisi.TabIndex = 6;
+            this.buttonBrisi.Text = "Brisi";
+            this.buttonBrisi.UseVisualStyleBackColor = true;
+            this.buttonBrisi.Click += new System.EventHandler(this.buttonBrisi_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(127, 403);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 102;
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(1182, 399);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Godina";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(779, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 32);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Odeljenje";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1060, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 32);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Ucenik";
+            // 
+            // Upisnica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 844);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBrisi);
+            this.Controls.Add(this.buttonIzmeni);
+            this.Controls.Add(this.buttonDodaj);
+            this.Controls.Add(this.comboBoxUcenik);
+            this.Controls.Add(this.comboBoxOdeljenje);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.comboBoxGodina);
+            this.Name = "Upisnica";
+            this.Text = "Upisnica";
+            this.Load += new System.EventHandler(this.Upisnica_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxGodina;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.ComboBox comboBoxOdeljenje;
+        private System.Windows.Forms.ComboBox comboBoxUcenik;
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonIzmeni;
+        private System.Windows.Forms.Button buttonBrisi;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ ednevnik/Upisnica.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ednevnik
+{
+    public partial class Upisnica : Form
+    {
+        DataTable dtOdeljenje, dtUpisnica;
+        public Upisnica()
+        {
+            InitializeComponent();
+        }
+
+        private void Upisnica_Load(object sender, EventArgs e)
+        {
+            comboGodinaPopuni();
+            comboOdeljenjePopuni();
+            comboBoxUcenik.Enabled = false;
+            comboUcenikPopuni();
+        }
+        private void comboGodinaPopuni()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from skolska_godina", veza);
+            DataTable dtGodina = new DataTable();
+            adapter.Fill(dtGodina);
+            comboBoxGodina.DataSource = dtGodina;
+            comboBoxGodina.ValueMember = "id";
+            comboBoxGodina.DisplayMember = "naziv";
+            comboBoxGodina.SelectedValue = 2;
+
+        }
+        private void comboBoxGodina_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (comboBoxGodina.IsHandleCreated && comboBoxGodina.Focused)
+            {
+                comboOdeljenjePopuni();
+                while (dataGridView1.Rows.Count > 0)
+                {
+                    dataGridView1.Rows.Remove(dataGridView1.Rows[0]);
+                }
+                textBoxId.Text = "";
+                comboBoxUcenik.SelectedIndex = -1;
+                comboBoxUcenik.Enabled = false;
+            }
+        }
+        private void comboOdeljenjePopuni()
+        {
+            string godina = comboBoxGodina.SelectedValue.ToString();
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("select id, str(razred) + '-' + indeks as naziv from odeljenje where godina_id = " + godina, veza);
+            dtOdeljenje = new DataTable();
+            adapter.Fill(dtOdeljenje);
+            comboBoxOdeljenje.DataSource = dtOdeljenje;
+            comboBoxOdeljenje.ValueMember = "id";
+            comboBoxOdeljenje.DisplayMember = "naziv";
+            comboBoxOdeljenje.SelectedIndex = -1;
+        }
+
+        private void comboBoxOdeljenje_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (comboBoxOdeljenje.IsHandleCreated && comboBoxOdeljenje.Focused)
+            {
+                comboUcenikPopuni();
+                comboBoxUcenik.Enabled = true;
+                comboBoxUcenik.SelectedIndex = -1;
+                textBoxId.Text = "";
+                gridPopuni();
+            }
+        }
+
+        private void gridPopuni()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("select upisnica.id, ime + ' ' + prezime as 'ime i prezime', osoba.id as ucenik from upisnica join osoba on osoba.id = osoba_id where odeljenje_id = " + comboBoxOdeljenje.SelectedValue.ToString(), veza); ;
+            dtUpisnica = new DataTable();
+            adapter.Fill(dtUpisnica);
+            dataGridView1.DataSource = dtUpisnica;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.Columns["ucenik"].Visible = false;
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            StringBuilder naredba = new StringBuilder("insert into upisnica (odeljenje_id, osoba_id) values('");
+            naredba.Append(comboBoxOdeljenje.SelectedValue.ToString() + "', '");
+            naredba.Append(comboBoxUcenik.SelectedValue.ToString() + "')");
+
+            SqlConnection veza = Konekcija.Connect();
+            SqlCommand komanda = new SqlCommand(naredba.ToString(), veza);
+
+            try
+            {
+                veza.Open();
+                komanda.ExecuteNonQuery();
+                veza.Close();
+                gridPopuni();
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message);
+            }
+        }
+
+        private void buttonIzmeni_Click(object sender, EventArgs e)
+        {
+            StringBuilder naredba = new StringBuilder("update upisnica set ");
+            naredba.Append("osoba_id = " + comboBoxUcenik.SelectedValue.ToString() + " ");
+            naredba.Append("where id = " + textBoxId.Text);
+
+            SqlConnection veza = Konekcija.Connect();
+            SqlCommand komanda = new SqlCommand(naredba.ToString(), veza);
+
+            try
+            {
+                veza.Open();
+                komanda.ExecuteNonQuery();
+                veza.Close();
+                gridPopuni();
+
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message);
+            }
+        }
+
+        private void buttonBrisi_Click(object sender, EventArgs e)
+        {
+            string naredba = "delete from upisnica where id = " + textBoxId.Text;
+
+            SqlConnection veza = Konekcija.Connect();
+            SqlCommand komanda = new SqlCommand(naredba, veza);
+
+            try
+            {
+                veza.Open();
+                komanda.ExecuteNonQuery();
+                veza.Close();
+                gridPopuni();
+
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message);
+            }
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int brojSloga = dataGridView1.CurrentRow.Index;
+                if (dtUpisnica.Rows.Count != 0 && brojSloga >= 0)
+                {
+                    comboBoxUcenik.SelectedValue = dataGridView1.Rows[brojSloga].Cells["Ucenik"].Value.ToString();
+                    textBoxId.Text = dataGridView1.Rows[brojSloga].Cells["id"].Value.ToString();
+                }
+            }
+        }
+
+        private void comboUcenikPopuni()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("select id, ime + ' ' + prezime as naziv from osoba where uloga = 1", veza);
+            DataTable dtUcenik = new DataTable();
+            adapter.Fill(dtUcenik);
+            comboBoxUcenik.DataSource = dtUcenik;
+            comboBoxUcenik.ValueMember = "id";
+            comboBoxUcenik.DisplayMember = "naziv";
+        }
+
+    }
+}
